15a832d5b3524a03cb11eb2369ba0d2458af158a
verenasworld <verena.felizidas@gmail.com>
2021-10-24 11:13:34 +0200

    import Title and Meta for my first SEO steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Meta} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'myPortfolio';
+
+  constructor(private metaTagService: Meta) {
+  }
+
+  ngOnInit(): void {
+    this.metaTagService.addTags([
+      {name:'keywords', content:'this is working very well'},
+      {name:'author', content:'VS'},
+      {name:'keywords', content:'this is working very well'},
+
+    ])
+  }
 }
